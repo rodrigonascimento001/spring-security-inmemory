f6cf40186e10d9ab7d97f3a3e1c69b56786fa485
Rodrigo Nascimento <rodrigo_allexandre@yahoo.com.br>
2022-08-10 09:11:01 -0300

    initial commit

---
+++ src/test/java/com/br/rodrigo/springsecurityinmemory/SpringSecurityInmemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.rodrigo.springsecurityinmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInmemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
